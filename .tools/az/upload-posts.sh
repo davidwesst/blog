38699a93466a5cf8abba9657ca3cc9b87c3dddda
David Wesst <david@cocobokostudios.com>
2022-06-18 20:47:57 +0000

    attempting to publish posts to blob storage

--- .tools/az/upload-posts.sh
+++ .tools/az/upload-posts.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-AZURE_STORAGE_CONNECTION_STRING=$1
-az storage blob upload-batch -d blog -s . --connection-string $AZURE_STORAGE_CONNECTION_STRING --pattern "[!.]*"
+CONN_STRING=$1
+az storage blob upload-batch -d blog -s . --overwrite --connection-string $CONN_STRING --pattern "[!.]*"
