5f393b0875f070bb860978dbd8ea71ca934adfc4
David Wesst <david@cocobokostudios.com>
2022-06-29 18:40:03 +0000

    extracting metadata frontmatter

--- .tools/az/publish.sh
+++ .tools/az/publish.sh
@@ -4,10 +4,42 @@
 # dependencies: git, yq, az
 
 # post_metadata (slug [folder name], title, description, excerpt, lastModifiedDate, publishedDate, tags)
+extract_title() {
+    local file=$1
+
+    echo $(yq --front-matter=extract '.title' $file) 
+}
+
+extract_publish_date() {
+    local file=$1
+
+    echo $(date --date `yq --front-matter=extract '.date' $file`) 
+}
+
+extract_last_modified_date() {
+    echo $(extract_publish_date $1)
+}
+
+extract_description() {
+    local file=$1
+
+    echo $(date --date `yq --front-matter=extract '.date' $file`) 
+}
+
+extract_tags() {
+    local file=$1
+
+    echo $(yq --front-matter=extract --output-format=csv '.tags' $file) 
+}
 
 # create array of post data
 for file in $(find */index.md); do
-    git log -n 1 --pretty="$(echo $file) %ci" $file
+    BLOG_TITLE=$(extract_title $file)
+    BLOG_PUBLISH=$(extract_publish_date $file)
+    BLOG_LAST_MODIFIED=$(extract_last_modified_date $file)
+    BLOG_DESCRIPTION=$(extract_description $file)
+    BLOG_TAGS=$(extract_tags $file)
+    echo $BLOG_TITLE 
 done
 
 # get all existing posts <slug>/index.md and metadata
