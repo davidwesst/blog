ba019300a500a61a0c5eb827cd7f8b3b0b145586
David Wesst <david@cocobokostudios.com>
2022-07-08 03:22:40 +0000

    publishing posts using child processes

--- .tools/az/publish.sh
+++ .tools/az/publish.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # dependencies: yq, az
 
@@ -36,7 +36,7 @@ extract_last_modified_date() {
 extract_description() {
     local file=$1
 
-    echo $(date --date `yq --front-matter=extract '.date' $file`) 
+    echo $(yq --front-matter=extract '.description' $file) 
 }
 
 extract_tags() {
@@ -45,36 +45,53 @@ extract_tags() {
     echo $(yq --front-matter=extract --output-format=csv '.tags' $file) 
 }
 
-# TODO: extract categories
-# TODO: extract excerpt from post content
-# TODO: calculate reading time and add as metadata
+publish_post() {
+	local file=$1
 
-# move into directory
-pushd $DIRECTORY
-
-# create array of post data
-for file in $(find */index.md); do
 	BLOG_SLUG=$(extract_slug $file)
     BLOG_TITLE=$(extract_title $file)
     BLOG_PUBLISH=$(extract_publish_date $file)
     BLOG_LAST_MODIFIED=$(extract_last_modified_date $file)
     BLOG_DESCRIPTION=$(extract_description $file)
     BLOG_TAGS=$(extract_tags $file)
-    echo $BLOG_TITLE $BLOG_SLUG
+	
+	echo "$BLOG_SLUG start!"
 
 	# upload directory contents
 	# TODO: Conditional upload logic to only upload new and updated items (based on md5content)
-	az storage blob upload-batch --destination blog/$BLOG_SLUG --source $BLOG_SLUG --connection-string $AZURE_STORAGE_CONNECTION_STRING --overwrite --pattern "[!.]*" 
+	az storage blob upload-batch \
+		--destination blog/$BLOG_SLUG \
+		--source $BLOG_SLUG \
+		--connection-string $AZURE_STORAGE_CONNECTION_STRING \
+		--overwrite \
+		--no-progress \
+		--pattern "[!._]*" > /dev/null 2>&1
 
 	# update metadata on index.md file
 	az storage blob metadata update \
 		--name $file \
 		--container-name "blog" \
 		--connection-string $AZURE_STORAGE_CONNECTION_STRING \
-		--metadata slug=$BLOG_SLUG title="$BLOG_TITLE" publishDate="$BLOG_PUBLISH" description="$BLOG_DESCRIPTION" tags="$BLOG_TAGS" 
+		--metadata slug=$BLOG_SLUG title="$BLOG_TITLE" publishDate="$BLOG_PUBLISH" description="$BLOG_DESCRIPTION" tags="$BLOG_TAGS" \
+		> /dev/null 2>&1
+
+	echo "$BLOG_SLUG complete!"
+}
+
+# TODO: extract categories
+# TODO: extract excerpt from post content
+# TODO: calculate reading time and add as metadata
+
+# move into directory
+pushd $DIRECTORY 1> /dev/null
+
+# create array of post data
+for file in $(find */index.md); do
+	publish_post $file &
 done
 
+wait
 # TODO: delete posts that are no longer present
 
 # pop directory
-popd
+popd 1> /dev/null
